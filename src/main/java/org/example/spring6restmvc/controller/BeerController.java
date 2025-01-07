f7200ba3d84387fc4aa11062b9618b79a0609f4f
vamshidhar <vamshidhar1001@gmail.com>
2025-01-06 19:09:06 -0800

    Implemented logs and written tests

---
+++ src/main/java/org/example/spring6restmvc/controller/BeerController.java
@@ -0,0 +1,24 @@
+package org.example.spring6restmvc.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.spring6restmvc.model.Beer;
+import org.example.spring6restmvc.services.BeerService;
+import org.springframework.stereotype.Controller;
+
+import java.util.UUID;
+
+@Slf4j
+@AllArgsConstructor
+@Controller
+public class BeerController {
+
+    private final BeerService beerService;
+
+    public Beer getBeerById(UUID id) {
+
+        log.debug("Get Beer by Id- in controller");
+
+        return beerService.getBeerById(id);
+    }
+}
