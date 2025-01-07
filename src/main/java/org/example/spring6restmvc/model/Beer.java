3012669f28d97881b2bdec09221d876dad2478cd
vamshidhar <vamshidhar1001@gmail.com>
2025-01-06 18:02:23 -0800

    Implemented the entity and attributes

---
+++ src/main/java/org/example/spring6restmvc/model/Beer.java
@@ -0,0 +1,23 @@
+package org.example.spring6restmvc.model;
+
+import lombok.Data;
+
+import java.beans.BeanInfo;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+@Data
+public class Beer {
+
+    private UUID id;
+    private Integer version;
+    private String beerName;
+    private BeerStyle beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+}
---
+++ src/main/java/org/example/spring6restmvc/model/BeerStyle.java
@@ -0,0 +1,6 @@
+package org.example.spring6restmvc.model;
+
+public enum BeerStyle {
+
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
